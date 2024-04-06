fb288105d565a90fad9bcf050a8847629436aee1
luyvannda <luyvannda@gmail.com>
2024-04-07 06:00:29 +0700

    extract ProfileDropDown to new component, clean up the code with Array.map and add icons to ProfileDropDown menu

---
+++ src/constants/index.ts
@@ -0,0 +1,15 @@
+export const navigation = [
+  { name: "Home", icon: "home", href: "#", current: true },
+  { name: "Messages", icon: "message", href: "#", current: false },
+  { name: "History", icon: "history", href: "#", current: false },
+  { name: "Tasks", icon: "task", href: "#", current: false },
+  { name: "Communities", icon: "groups", href: "#", current: false },
+];
+
+export const profileMenu = [
+  { name: "Your Profile", icon: "person", href: "#" },
+  { name: "Settings", icon: "settings", href: "#" },
+  { name: "Support", icon: "support_agent", href: "#" },
+  { name: "Privacy", icon: "lock", href: "#" },
+  { name: "Sign Out", icon: "logout", href: "#" },
+];
